e867fc9453ab10c108c1edde8f4e3e78c759f77a
ᴀᴋᴀʀɪ <akariondev@kamisatoofficial.xyz>
2022-09-16 03:22:20 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 apt update; apt upgrade -y; pkg in nodejs-lts git python neovim -y && pip install --upgrade pip; pip install wheel pynvim neovim && mkdir .config; cd .config; mkdir nvim; cd && termux-setup-storage && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && git clone https://github.com/Akaritsu/nvim/; cd nvim; cp init.vim /data/data/com.termux/files/home/.config/nvim/; cd; rm -rf nvim && bash chmod +x download.sh;
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && git clone https://github.com/Akaritsu/nvim/; cd nvim; cp init.vim /data/data/com.termux/files/home/.config/nvim/; cd; rm -rf nvim \
+   bash chmod +x download.sh;
