12595809d872ccf15856518dc158ab6855b97722
ᴀᴋᴀʀɪ <akariondev@gmail.com>
2022-09-19 23:16:34 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -36,15 +36,18 @@ for distrosUbuntu in baseUbuntu["derived"]:
 
 for distrosArch in baseArch["derived"]:
     try:
+
         name = open('/etc/issue').readline()
         if distrosArch in name:
             print(name + "System based on Arch Detected.")
-            system('''pacman -Syu; pacman -Syu nodejs-lts-gallium git python neovim && pip install --upgrade pip; pip install wheel; pip install pynvim neovim && mkdir .config; cd .config; mkdir nvim; cd && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim''')
-        elif distrosArch is None:
-            pass
+            system(r'''pacman -Syu; pacman -Syu nodejs-lts-gallium git python neovim; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim''')
+        
+         elif distrosArch is None:
+             pass
 
     except Exception as e:
+        print("error.")
         quit()
 
 
