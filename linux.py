21fd6e7913cea5c5d1f1bd137326c202bce13e0a
ᴀᴋᴀʀɪ <akariondev@gmail.com>
2022-09-20 14:46:00 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -26,7 +26,7 @@ for distrosUbuntu in baseUbuntu["derived"]:
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim; rm linux.py; clear''')
             system("clear")
-            system("""echo 'Received: ‘Distro not supported’.
+            system(r"""echo 'Received: ‘Distro not supported’.
 
 If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial or https://t.me/AkariOficial .'""")
 
@@ -47,7 +47,7 @@ for distrosArch in baseArch["derived"]:
             system(r'''pacman -Syu; pacman -Syu npm; pacman -S python-pip; pacman -Syu nodejs-lts-gallium git python neovim; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim; rm linux.py''')
             system("clear")
-            system("""echo 'Received: ‘Distro not supported’.
+            system(r"""echo 'Received: ‘Distro not supported’.
 
 If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial or https://t.me/AkariOficial .'""")
         elif distrosArch is None:
