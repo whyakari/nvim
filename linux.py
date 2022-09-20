d2ec7326a32e08a69e1fdd027678f35951ea22c6
ᴀᴋᴀʀɪ <akariondev@gmail.com>
2022-09-20 00:18:37 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -24,7 +24,10 @@ for distrosUbuntu in baseUbuntu["derived"]:
             system(r'''apt update; apt upgrade -y; apt curl install git python3 neovim -y; curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash; nvm install v16.2.0; export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim''')
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim; rm linux.py; clear''')
+            system("""echo 'Received: ‘Distro not supported’.
+
+If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial or https://t.me/AkariOficial .'""")
 
         elif distrosUbuntu is None:
             pass
@@ -41,13 +44,13 @@ for distrosArch in baseArch["derived"]:
         if distrosArch in name:
             print(name + "System based on Arch Detected.")
             system(r'''pacman -Syu; pacman -Syu npm; pacman -Syu nodejs-lts-gallium git python neovim; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim''')
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim; rm linux.py''')
+            system("""echo 'Received: ‘Distro not supported’.
+
+If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial or https://t.me/AkariOficial .'""")
         elif distrosArch is None:
             pass
 
     except Exception as e:
         print("error.")
         quit()
-
-
-#print(name + """Distro not supported. If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial.""")
