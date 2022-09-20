6a6bacd3f110e683d51f2bc237190c619273c036
ᴀᴋᴀʀɪ <akariondev@gmail.com>
2022-09-19 23:09:37 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -31,8 +31,6 @@ for distrosUbuntu in baseUbuntu["derived"]:
 
     except Exception as e:
         print("error, problaby is using termux.")
-        system('''pacman -Syu; pacman -Syu nodejs-lts-gallium git python neovim && pip install --upgrade pip; pip install wheel; pip install pynvim neovim && mkdir .config; cd .config; mkdir nvim; cd && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim''')
         quit()
 
 
@@ -41,6 +39,8 @@ for distrosArch in baseArch["derived"]:
         name = open('/etc/issue').readline()
         if distrosArch in name:
             print(name + "System based on Arch Detected.")
+            system('''pacman -Syu; pacman -Syu nodejs-lts-gallium git python neovim && pip install --upgrade pip; pip install wheel; pip install pynvim neovim && mkdir .config; cd .config; mkdir nvim; cd && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim''')
         elif distrosArch is None:
             pass
 
