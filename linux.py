d91b9d158823033b5bd6f35dd4e2fef4c1bbff28
ᴀᴋᴀʀɪ <akariondev@gmail.com>
2022-09-20 00:23:32 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -25,7 +25,8 @@ for distrosUbuntu in baseUbuntu["derived"]:
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim; rm linux.py; clear''')
-            system("""clear; echo 'Received: ‘Distro not supported’.
+            system("clear")
+            system("""echo 'Received: ‘Distro not supported’.
 
 If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial or https://t.me/AkariOficial .'""")
 
@@ -45,7 +46,8 @@ for distrosArch in baseArch["derived"]:
             print(name + "System based on Arch Detected.")
             system(r'''pacman -Syu; pacman -Syu npm; pacman -Syu nodejs-lts-gallium git python neovim; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /root/.config/nvim/; cd; rm -rf nvim; rm linux.py''')
-            system("""clear; echo 'Received: ‘Distro not supported’.
+            system("clear")
+            system("""echo 'Received: ‘Distro not supported’.
 
 If you believe what your is distro Supported, then contact-me in Telegram @AkariOficial or https://t.me/AkariOficial .'""")
         elif distrosArch is None:
