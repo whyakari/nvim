1196aec9f1889eefeb9a6679c1de66dc7a95e9fa
Akari <akariondev@gmail.com>
2023-03-16 20:15:34 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -10,11 +10,10 @@ baseArch = baseds(derived=data)
 
 for distrosArch in baseArch["derived"]:
     try:
-
         name = open('/etc/issue').readline()
         if distrosArch in name:
             print(name + "System based on Arch Detected.")
-            system(r'''sudo pacman -Syu; sudo pacman -Syu npm; sudo pacman -S python-pip; sudo pacman -Syu nodejs-lts-gallium git python neovim; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+            system(r'''sudo pacman -Syu --noconfirm; sudo pacman -Syu npm --noconfirm; sudo pacman -S python-pip --noconfirm; sudo pacman -Syu nodejs-lts-gallium git python neovim --noconfirm; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim $HOME/.config/nvim/; cd; rm -rf nvim; rm linux.py;''')
             system("clear")
             system(r"""echo 'Received: ‘Distro not supported’.
@@ -25,4 +24,3 @@ If you believe what your is distro Supported, then contact-me in Telegram @Akari
 
     except Exception as e:
         print("error.")
-        quit()
