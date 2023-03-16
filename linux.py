df8409a712fb35d07ce106dde760aedd8993afae
Akari <akariondev@gmail.com>
2023-03-16 20:22:50 -0300

    Update linux.py

--- linux.py
+++ linux.py
@@ -13,7 +13,7 @@ for distrosArch in baseArch["derived"]:
         name = open('/etc/issue').readline()
         if distrosArch in name:
             print(name + "System based on Arch Detected.")
-            system(r'''sudo pacman -Syu --noconfirm; sudo pacman -Syu npm --noconfirm; sudo pacman -S python-pip --noconfirm; sudo pacman -Syu nodejs-lts-gallium git python neovim --noconfirm; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+            system(r'''sudo pacman -Syu --noconfirm; sudo pacman -Syu npm sudo python-pip nodejs-lts-gallium git python neovim --noconfirm; pip install --upgrade pip; pip install wheel; pip install pynvim neovim; mkdir .config; cd .config; mkdir nvim; cd; sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim $HOME/.config/nvim/; cd; rm -rf nvim; rm linux.py;''')
             system("clear")
             system(r"""echo 'Received: ‘Distro not supported’.
