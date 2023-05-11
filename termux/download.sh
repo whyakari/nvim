fbcf60091c4f9fc66c32d612f78c7be311ba8417
Akari Chan <akariondev@gmail.com>
2023-05-11 04:52:35 +0000

    added folder for termux separate

---
+++ termux/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt update; apt upgrade -y; pkg in nodejs-lts git python neovim -y && pip install --upgrade pip; pip install wheel pynvim neovim && mkdir .config; cd .config; mkdir nvim; cd && termux-setup-storage && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && git clone https://github.com/AkariOficial/nvim/; cd nvim; cp init.vim /data/data/com.termux/files/home/.config/nvim/; cd; rm -rf nvim \
+   bash chmod +x download.sh; rm download.sh
