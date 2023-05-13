2142524217f310e439cc7973ab8fb060b62cacd2
Akari Chan <akariondev@gmail.com>
2023-05-13 20:59:56 +0000

    Fix: Fixed error when cloning the Neovim repository
    When trying to clone the Neovim repository, the installation script was failing due to the destination path '/root/nvim' already existing and not being empty. To solve this problem, the command 'rm -rf /root/nvim' was added to delete the directory before cloning the repository again.

--- main.go
+++ main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
+        "fmt"
+        "os/exec"
 )
 
 func main() {
-    // commands for executeon terminal.
-	cmds := []string{
-		"sudo pacman -Syu --noconfirm",
-		"sudo pacman -Syu npm sudo python-pip nodejs-lts-gallium git python neovim --noconfirm",
-		"pip install --upgrade pip",
-		"pip install wheel",
-		"pip install pynvim neovim",
-		"mkdir -p $HOME/.config/nvim",
-		"curl -fLo \"$HOME/.local/share/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
-		"git clone https://github.com/AkariOficial/nvim $HOME/nvim",
-		"cp $HOME/nvim/init.vim $HOME/.config/nvim/",
-		"rm -rf $HOME/nvim",
-	}
+        cmds := []string{
+                "sudo pacman -Syu --noconfirm",
+                "sudo pacman -Syu npm sudo python-pip nodejs-lts-gallium git python neovim --noconfirm",
+                "pip install --upgrade pip",
+                "pip install wheel",
+                "pip install pynvim neovim",
+                "mkdir -p $HOME/.config/nvim",
+                "curl -fLo \"$HOME/.local/share/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+                "rm -rf $HOME/nvim",
+                "git clone -v https://github.com/AkariOficial/nvim $HOME/nvim",
+                "cp $HOME/nvim/init.vim $HOME/.config/nvim/",
+                "rm -rf $HOME/nvim",
+        }
 
-	// run each command in the terminal
-	for _, cmd := range cmds {
-		err := exec.Command("bash", "-c", cmd).Run()
-		if err != nil {
-			fmt.Println("error when executing command:", cmd)
-			break
-		}
-	}
+        for _, cmd := range cmds {
+                err := exec.Command("bash", "-c", cmd).Run()
+                if err != nil {
+                        fmt.Println("error when executing command:", cmd)
+                        break
+                }
+        }
 }
 
