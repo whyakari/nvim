54012e1670764afe5ee8102baf9c072661e8d556
Akari Chan <akariondev@gmail.com>
2023-05-17 17:29:59 +0000

    removed sudo

--- pkg/ubuntu/ubuntu.go
+++ pkg/ubuntu/ubuntu.go
@@ -7,12 +7,12 @@ import (
 
 func UbuntuCommands() []string {
 	return []string{
-		"sudo apt-get update",
-		"sudo apt-get upgrade -y",
-		"sudo apt-get install -y npm sudo python3-pip nodejs git python3-neovim",
-		"pip3 install --upgrade pip",
-		"pip3 install wheel",
-		"pip3 install pynvim neovim",
+		"apt update",
+		"apt upgrade -y",
+		"apt install -y npm python3-pip nodejs git python3-neovim",
+		"pip install --upgrade pip",
+		"pip install wheel",
+		"pip install pynvim neovim",
 		"mkdir -p $HOME/.config/nvim",
 		"curl -fLo \"$HOME/.local/share/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
 		"rm -rf $HOME/nvim",
