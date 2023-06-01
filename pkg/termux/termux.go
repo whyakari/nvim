de3876e55420ff65e2a2508c5df4eeae4bcc85fd
Akari Chan <akariondev@gmail.com>
2023-06-01 17:01:43 +0000

    added install nvim for termux users Add Termux Commands() and pkg/termux/termux.go function to automate the configuration of the Termux environment with Neovim packages and plugins.

---
+++ pkg/termux/termux.go
@@ -0,0 +1,33 @@
+package arch
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func IsTermux() bool {
+	out, err := exec.Command("bash", "-c", "uname -o").Output()
+	if err != nil {
+		return false
+	}
+    return strings.TrimSpace(string(out)) == "Android"
+}
+
+func TermuxCommands() []string {
+	return []string{
+		"apt update",
+		"apt upgrade -y",
+		"pkg install nodejs-lts git python neovim -y",
+		"pip install --upgrade pip",
+		"pip install wheel pynvim neovim",
+		"mkdir -p $HOME/.config/nvim",
+		"curl -fLo \"$HOME/.local/share/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+		"termux-setup-storage",
+		"git clone -v https://github.com/AkariOficial/nvim $HOME/nvim",
+		"cp $HOME/nvim/init.vim $HOME/.config/nvim/",
+		"rm -rf $HOME/nvim",
+		"rm $HOME/README.md",
+        "rm $HOME/*.tar.gz",
+	}
+}
+
