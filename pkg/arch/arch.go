805fd8ee5a8b3f0d03443eee16145e784bd742b2
Akari Chan <akariondev@gmail.com>
2023-06-21 20:28:31 +0000

    [Update] Arch Linux Privilege and Command Management This update brings improvements to the privilege and command management in the Arch Linux script. The script now checks if the user has root permissions and acts accordingly. If the user is root, commands that require root privileges are removed from the list. Otherwise, the script prompts the user for the sudo password and adds `sudo -S` before the commands that require root privileges.

--- pkg/arch/arch.go
+++ pkg/arch/arch.go
@@ -1,20 +1,53 @@
 package arch
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"strings"
+	"syscall"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-func IsArchLinux() bool {
-	out, err := exec.Command("bash", "-c", "cat /etc/os-release | grep -oP '(?<=^ID=).+'").Output()
-	if err != nil {
-		return false
+func isRoot() bool {
+	return os.Geteuid() == 0
+}
+
+func addSudoToCommands(commands []string, password string) []string {
+	sudoCommands := make([]string, len(commands))
+	for i, command := range commands {
+		sudoCommands[i] = "echo " + password + " | sudo -S " + command
 	}
-    return strings.TrimSpace(string(out)) == "archarm"
+	return sudoCommands
+}
+
+func removeRootCommands(commands []string) []string {
+	rootCommands := []string{
+		"pacman -Syu --noconfirm",
+		"pacman -Syu npm sudo python-pip nodejs-lts-gallium git python neovim --noconfirm",
+	}
+
+	filteredCommands := make([]string, 0)
+	for _, command := range commands {
+		if !contains(rootCommands, command) {
+			filteredCommands = append(filteredCommands, command)
+		}
+	}
+
+	return filteredCommands
+}
+
+func contains(slice []string, value string) bool {
+	for _, item := range slice {
+		if item == value {
+			return true
+		}
+	}
+	return false
 }
 
 func ArchCommands() []string {
-	return []string{
+	commands := []string{
 		"pacman -Syu --noconfirm",
 		"pacman -Syu npm sudo python-pip nodejs-lts-gallium git python neovim --noconfirm",
 		"pip install --upgrade pip --break-system-packages",
@@ -25,9 +58,34 @@ func ArchCommands() []string {
 		"rm -rf $HOME/nvim",
 		"git clone -v https://github.com/whyakari/nvim $HOME/nvim",
 		"cp $HOME/nvim/init.vim $HOME/.config/nvim/",
-        "cd $HOME",
+		"cd $HOME",
 		"rm -rf $HOME/nvim",
-        "rm $HOME/README.md",
-        "rm $HOME/*.tar.gz",
+		"rm $HOME/README.md",
+		"rm $HOME/*.tar.gz",
 	}
+
+	if !isRoot() {
+		fmt.Print("Senha do sudo: ")
+		bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			fmt.Println("\nErro ao ler a senha do sudo:", err)
+			os.Exit(1)
+		}
+		password := string(bytePassword)
+
+		commands = addSudoToCommands(commands, password)
+	} else {
+		commands = removeRootCommands(commands)
+	}
+
+	return commands
 }
+
+func IsArchLinux() bool {
+	out, err := exec.Command("bash", "-c", "cat /etc/os-release | grep -oP '(?<=^ID=).+'").Output()
+	if err != nil {
+		return false
+	}
+	return strings.TrimSpace(string(out)) == "archarm"
+}
+
