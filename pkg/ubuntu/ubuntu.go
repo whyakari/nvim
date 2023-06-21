15b07880fb8d4b7a9df019979297daff30bdd915
Akari <akariondev@gmail.com>
2023-06-21 16:41:11 -0300

    according to the PEP 668
    read the other commits

--- pkg/ubuntu/ubuntu.go
+++ pkg/ubuntu/ubuntu.go
@@ -9,13 +9,13 @@ func UbuntuCommands() []string {
 	return []string{
 		"apt update",
 		"apt install -y curl npm python3-pip nodejs git python3-neovim",
-		"pip install --upgrade pip",
-		"pip install wheel",
-		"pip install pynvim neovim",
+		"pip install --upgrade pip --break-system-packages",
+		"pip install wheel --break-system-packages",
+		"pip install pynvim neovim --break-system-packages",
 		"mkdir -p $HOME/.config/nvim",
 		"curl -fLo \"$HOME/.local/share/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
 		"rm -rf $HOME/nvim",
-		"git clone -v https://github.com/AkariOficial/nvim $HOME/nvim",
+		"git clone -v https://github.com/whyakari/nvim $HOME/nvim",
 		"cp $HOME/nvim/init.vim $HOME/.config/nvim/",
 		"rm -rf $HOME/nvim",
 	}
