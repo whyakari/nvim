d0c24ef79fbbaa18be0dd3a5993b192ddfd5e925
Akari Chan <akariondev@gmail.com>
2023-06-21 21:32:48 +0000

    update

--- pkg/arch/arch.go
+++ pkg/arch/arch.go
@@ -16,18 +16,18 @@ func IsArchLinux() bool {
 func ArchCommands() []string {
 	return []string{
 		"sudo pacman -Syu --noconfirm",
-		"pacman -Syu npm sudo python-pip nodejs-lts-gallium git python neovim --noconfirm",
+		"sudo pacman -Syu npm python-pip nodejs-lts-gallium git python neovim --noconfirm",
 		"pip install --upgrade pip --break-system-packages",
 		"pip install wheel --break-system-packages",
 		"pip install pynvim neovim --break-system-packages",
-		"mkdir -p $HOME/.config/nvim",
+		"sudo mkdir -p $HOME/.config/nvim",
 		"curl -fLo \"$HOME/.local/share/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
-		"rm -rf $HOME/nvim",
+		"sudo rm -rf $HOME/nvim",
 		"git clone -v https://github.com/whyakari/nvim $HOME/nvim",
-		"cp $HOME/nvim/init.vim $HOME/.config/nvim/",
+		"sudo cp $HOME/nvim/init.vim $HOME/.config/nvim/",
         "cd $HOME",
-		"rm -rf $HOME/nvim",
-        "rm $HOME/README.md",
-        "rm $HOME/*.tar.gz",
+		"sudo rm -rf $HOME/nvim",
+        "sudo rm $HOME/README.md",
+        "sudo rm $HOME/*.tar.gz",
 	}
 }
